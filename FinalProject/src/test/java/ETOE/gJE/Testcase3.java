932bfb16686badc0eaa9b8ff32f0cf5613bd41bf
Seema Shukla <Seema Shukla@Seema_Shukla>
2021-10-18 18:22:05 +0530

    ddd

---
+++ FinalProject/src/test/java/ETOE/gJE/Testcase3.java
@@ -0,0 +1,29 @@
+package ETOE.gJE;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Testcase3 {
+	
+	
+	
+	
+	
+	@Test 
+	public void Testcase() throws InterruptedException {
+	
+	
+	 System.setProperty("webdriver.chrome.driver","D:\\chromedriver_win32 (1)\\chromedriver.exe");
+	 WebDriver driver = new ChromeDriver();
+    driver.manage().window().maximize();
+    driver.get("https://www.facebook.com/");
+    Thread.sleep(2000);
+    driver.findElement(By.id("email")).sendKeys("yourusername@gmail.com");
+    driver.findElement(By.id("pass")).sendKeys("password");
+    driver.quit();
+}
+}
